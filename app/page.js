1f4be0ab9b3d183368e99117a3ed2fbbce6e7584
Nissrine Canina <niss.canina@gmail.com>
2023-04-13 22:06:06 -0400

    importd Movie

--- app/page.js
+++ app/page.js
@@ -1,3 +1,4 @@
+import Movie from "./Movie"
 
 export default async function Home() {
 
@@ -5,10 +6,21 @@ export default async function Home() {
 
   const res = await data.json()
   console.log(res)
-  
+
   return (
     <main>
       <h1 className="text-lg py-4 px-4">Hello LOGO</h1>
+      <div className="grid gap-16 grid-cols-fluid">
+      {res.results.map(movie => (
+        <Movie 
+          key={movie.id}
+          id={movie.id}
+          title={movie.title}
+          poster_path={movie.poster_path}
+          release_date={movie.release_date}
+        />
+      ))}
+      </div>
       
     </main>
   )
